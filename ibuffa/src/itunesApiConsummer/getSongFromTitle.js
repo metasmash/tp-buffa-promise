6d3e6bc0c8c0cbd37a282ac7b619cc91df7690a6
OUALI Yannick <oualiyannick@hotmail.fr>
2021-03-17 17:27:33 +0100

    Update getSongFromTitle.js

--- ibuffa/src/itunesApiConsummer/getSongFromTitle.js
+++ ibuffa/src/itunesApiConsummer/getSongFromTitle.js
@@ -7,12 +7,12 @@ const baseURL = 'https://itunes.apple.com/search?'
 //example of term
 //const term='the+pot'
 
-const params = (term) => {country:'fr',media:'music',term}
+const params = (term) => ({ country: 'fr', media: 'music', term })
 
 export const getMusicFromSearch = async (search) => {
     try {
         const { data } = await formatSearch(search).then((x) =>
-            axios.get(baseURL, { withCredentials: true, params(search) })
+            axios.get(baseURL, { withCredentials: true, ...params(search) })
         )
 
         return _.map(data.results, destructureResults)
